1c395aa6884f4033d2545812912536e9737fe134
Vladimir Romashchenko <eaglesemanation@gmail.com>
2022-08-24 00:31:24 -0400

    Args parsing basics

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,66 @@
-use clap::Parser;
+use std::{time::Duration, str::FromStr};
 
-#[derive(Parser, Debug)]
+use clap::{Parser, Subcommand, Args};
+use tokio_serial::{available_ports, SerialPortBuilderExt};
+use parse_duration::parse::{parse as parse_dur, Error as ParseDurationError};
+
+#[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
-struct Args {
-    #[clap(short, long, value_parser)]
-    message: Option<String>,
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
 }
 
-fn main() {
-    let args = Args::parse();
+#[derive(Subcommand)]
+enum Commands {
+    /// Lists connected serial devices
+    List,
+    /// Gets readings from spectrometer
+    Read(ReadingConf),
+}
 
-    match args.message {
-        Some(msg) => println!("{}", msg),
-        None => println!("Hello world!"),
+struct ParsableDuration(Duration);
+
+impl FromStr for ParsableDuration {
+    type Err = ParseDurationError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        parse_dur(s).map(|d| ParsableDuration(d))
     }
+}
+
+#[derive(Args)]
+struct ReadingConf {
+    /// Name of serial port that should be used
+    serial: String,
+    /// How long to scan for
+    duration: ParsableDuration,
+    /// Serial port baud rate
+    baud_rate: u32
+}
+
+#[tokio::main]
+async fn main() {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::List => list_serial().expect("Getting list of serial ports failed"),
+        Commands::Read(conf) => get_readings(conf).await.expect("Reading from serial failed"),
+    }
+}
+
+fn list_serial() -> tokio_serial::Result<()> {
+    let ports = available_ports()?;
+    ports.iter().for_each(|port| println!("{}", port.port_name));
+
+    Ok(())
+}
+
+async fn get_readings(conf: &ReadingConf) -> tokio_serial::Result<()> {
+    let mut port = tokio_serial::new(conf.serial.clone(), conf.baud_rate).open_native_async()?;
+    /*
+    #[cfg(unix)]
+    port.set_exclusive(false)?;
+    */
+
+    Ok(())
 }
