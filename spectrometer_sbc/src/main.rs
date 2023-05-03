92a4228a8b1e48a2ae0a55a6b48f2aea8959b0a4
Vladimir Romashchenko <eaglesemanation@gmail.com>
2023-05-03 02:34:14 -0400

    fix: Add vendoring for cargo project with local site method

--- spectrometer_sbc/src/main.rs
+++ spectrometer_sbc/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use rppal::gpio::Gpio;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let gpio = Gpio::new()?;
+
+    let mut led = gpio.get(23)?.into_output();
+    let mut button = gpio.get(21)?.into_input_pullup();
+
+    button.set_interrupt(rppal::gpio::Trigger::RisingEdge)?;
+
+    for _ in 0..10 {
+        button.poll_interrupt(false, None)?;
+        led.toggle();
+    }
+
+    Ok(())
 }
